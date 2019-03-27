b0232af53caf85958d90bebd193ebf8aaa3715a0
My Hanh <hanh05111998@gmail.com>
2019-03-27 11:01:40 +0700

    làm header

---
+++ public/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../public/css/style.css">
+</head>
+<body>
+<header id="header">
+    <!-- Container -->
+    <div class="container ">
+        
+        <!-- Logo / Mobile Menu -->
+        <div class="three columns">
+            <div id="logo">
+                                    <h1><a href="index.html" title="Chow" rel="home"><img src="files/2015/02/logo2.png" alt="Chow"/></a></h1>
+                                                </div>
+        </div>
+
+
+        <!-- Navigation
+        ================================================== -->
+                <div class=" columns navigation thirteen">
+                    <nav id="navigation" class=" menu nav-collapse">
+                <ul id="menu-menu" class="menu"><li id="menu-item-18" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-18"><a href="index.html">Home</a></li>
+<li id="menu-item-40" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-40"><a href="browse-recipes/index.html">Browse Recipes</a></li>
+<li id="menu-item-25" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-25"><a href="#">Demos</a>
+<ul class="sub-menu">
+	<li id="menu-item-84" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-84"><a href="http://goo.gl/OIxVQD">Full-Width Version</a></li>
+	<li id="menu-item-32" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-32"><a href="boxed-version/index.html">Boxed Version</a></li>
+	<li id="menu-item-3024" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-3024"><a href="#">Post Layouts</a>
+	<ul class="sub-menu">
+		<li id="menu-item-3025" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3025"><a href="http://goo.gl/OIxVQD">Masonry Grid</a></li>
+		<li id="menu-item-33" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-33"><a href="list-homepage/index.html">List Grid</a></li>
+	</ul>
+</li>
+	<li id="menu-item-26" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-26"><a href="#">Recipe Styles</a>
+	<ul class="sub-menu">
+		<li id="menu-item-89" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-89"><a href="recipe/farmhouse-vegetable-and-barley-soup/index.html">Recipe Style #1</a></li>
+		<li id="menu-item-90" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-90"><a href="recipe/chunky-beef-stew/index.html">Recipe Style #2</a></li>
+	</ul>
+</li>
+</ul>
+</li>
+<li id="menu-item-27" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-27"><a href="#">Pages</a>
+<ul class="sub-menu">
+	<li id="menu-item-162" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-162"><a href="chow-dashboard/index.html">Chow Dashboard</a></li>
+	<li id="menu-item-23" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-23"><a href="shortcodes/index.html">Shortcodes</a></li>
+	<li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-24"><a href="typography/index.html">Typography</a></li>
+	<li id="menu-item-42" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-42"><a href="contact/index.html">Contact</a></li>
+</ul>
+</li>
+<li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-22"><a href="shop/index.html">Shop</a>
+<ul class="sub-menu">
+	<li id="menu-item-85" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-85"><a href="shop/index.html">Shop</a></li>
+	<li id="menu-item-86" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-86"><a href="product/chilli-powder/index.html">Product Page</a></li>
+	<li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19"><a href="cart/index.html">Shopping Cart</a></li>
+</ul>
+</li>
+<li id="menu-item-35" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><a href="submit-page/index.html">Submit Recipe</a></li>
+</ul>            </nav>
+
+        </div>
+        
+    </div>
+
+<!-- Container / End -->
+</header>
+    
+
